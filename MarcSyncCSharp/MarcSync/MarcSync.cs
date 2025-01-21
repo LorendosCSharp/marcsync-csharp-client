f7b5d334a57f53b7b0066fcd4bf08c7bd5242fb3
marcthedev <admin@marcthedev.it>
2025-01-21 11:02:01 +0100

    did some finalization on the methods

---
+++ MarcSyncCSharp/MarcSync/MarcSync.cs
@@ -0,0 +1,16 @@
+﻿namespace MarcSync;
+
+public class MarcSyncClient
+{
+    private readonly string accessToken;
+
+    public MarcSyncClient(string accessToken)
+    {
+        this.accessToken = accessToken;
+    }
+
+    public Collection GetCollection(string collectionName)
+    {
+        return new Collection(accessToken, collectionName);
+    }
+}
